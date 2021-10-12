0b32f78ed06413760dff40005799b96c6a37a761
Amehra2 <akashmehra2001@hotmail.com>
2021-10-12 14:51:40 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
+import ReactDom from 'react-dom';
+import './index.css';
+import App from '/App';
 import "./style.css";
 
-export default function App() {
+ReactDOM.render(
+  <App />,
+  document.getElementById('Root')
+);
+
+function Welcome(props){
+  return <h1>hello, {props.name}</h1>
+}
+function clickhandler(){
+  window.alert("Hello from React!");
+}
+
+function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+      <Welcome name=" Mike"/>
+      <Welcome name= "Sarah"/>
+      <h1 style= {{color:"red"}}> Some Text</h1>
+      <button onClick={clickhandler}>
+        show message
+        </button>
+
+
+        </div>
+
   );
+
+
+
+export default App;
+
 }
